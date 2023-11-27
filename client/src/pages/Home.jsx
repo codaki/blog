5abd590f4a38369322e8954255219a4df50c872f
Christopher Iza <cdiza5@espe.edu.ec>
2023-11-26 21:52:06 -0500

    Correccion

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,10 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import axios from "axios";
 
-const Home =()=>{
+const Home = () => {
   const [posts, setPosts] = useState([]);
-  const cat=useLocation().search
-  
+  const cat = useLocation().search;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,36 +16,33 @@ const Home =()=>{
       }
     };
     fetchData();
-  },[cat]);
+  }, [cat]);
 
-
-
-         const getText = (html) =>{
-          const doc = new DOMParser().parseFromString(html,"text/html")
-          return doc.body.textContent
-         }
-    return(
-        <div className="home">
-            <div className="posts">
-                {posts.map(post=>(
-                    <div className="post" key={post.id}>
-                        <div className="img">
-                            <img src={`../upload/${post.img}`} alt="" />
-                        </div>
-                        <div className="content">
-                            <Link className="link" to={`/post/${post.id}`}>                                
-                            <h1>{post.title}</h1>
-                            </Link>
-                            <p>{getText(post.desc)}</p>
-                            <Link className="link" to={`/post/${post.id}`}>
-                            <button>Lee Más</button>
-                            </Link>                            
-                        </div>
-                    </div>
-                ))}
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
+  return (
+    <div className="home">
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={`../upload/${post.img}`} alt="" />
             </div>
-        </div>
-    )
-
-}
-export default Home
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{getText(post.desc)}</p>
+              <Link className="link" to={`/post/${post.id}`}>
+                <button>Leer Más</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Home;
